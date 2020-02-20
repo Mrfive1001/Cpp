6d2d6c57531bf3053f099a3f1f392d6d3488efd5
mrfive1001 <13070156190@163.com>
2020-02-20 21:27:45 +0800

    add chapter 9

---
+++ Chapter9/coordin.h
@@ -0,0 +1,16 @@
+#pragma  once
+
+struct polar
+{
+    double distance;
+    double angle;
+};
+
+struct rect
+{
+    double x;
+    double y;
+};
+
+polar rect_to_polar(rect);
+void show_polar(polar);
---
+++ Chapter9/file2.cpp
@@ -0,0 +1,28 @@
+# include<iostream>
+# include"coordin.h"
+# include<cmath>
+using namespace std;
+
+polar rect_to_polar(rect xypos)
+{
+    polar result;
+    result.distance = sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
+    result.angle = atan2(xypos.y, xypos.x);
+    return result;
+}
+
+rect polar_to_rect(polar ppos)
+{
+    rect result;
+    result.x = ppos.distance * cos(ppos.angle);
+    result.y = ppos.distance * sin(ppos.angle);
+    return result;
+}
+
+void show_polar(polar ppos)
+{
+    const double Rad_to_deg =  57.3;
+    cout << "distance =  " << ppos.distance;
+    cout << " angle = " << ppos.angle*Rad_to_deg;
+    cout << "degrees\n";
+}
